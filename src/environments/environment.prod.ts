2bd4b763246df635f27246d9e6531de294e5713e
mmarquez <mmarquez@randstad.com.ar>
2024-08-20 23:08:39 -0300

    build: :construction_worker: Prod environment and script added

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  baseUrl: 'https://stock.ueingenieria.com/api',
+  apiKey: 'ca48accb-8d57-490c-9b98-a8e0bbe1d16e',
+  fBaseUrl: 'https://stock.ueingenieria.com/'
+};
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
